9ddb5b9a0b98da6d718bc4e6715eb39960924846
bojanzivkov <bojanzivkov@2374380.no-reply.drupal.org>
2017-07-11 13:59:58 +0200

    Issue #2815309 by bradjones1, Bojan Živkov: Migrate to JavaScript SDK 3.x

--- js/commerce_braintree.form.js
+++ js/commerce_braintree.form.js
@@ -19,21 +19,18 @@
    */
   Drupal.behaviors.commerceBraintreeForm = {
     attach: function (context) {
-      var $form = $('.braintree-form', context).closest('form');
+      var $form = $('.braintree-form', context).closest('form').once('braintree-attach');
       if ($form.length === 0) {
         return;
       }
 
-      var commerceBraintree = $form.data('braintree');
-      if (!commerceBraintree) {
-        var waitForSdk = setInterval(function () {
-          if (typeof braintree !== 'undefined') {
-            commerceBraintree = new Drupal.commerceBraintree($form, drupalSettings.commerceBraintree);
-            $form.data('braintree', commerceBraintree);
-            clearInterval(waitForSdk);
-          }
-        }, 100);
-      }
+      var waitForSdk = setInterval(function () {
+        if (typeof braintree !== 'undefined') {
+          var commerceBraintree = new Drupal.commerceBraintree($form, drupalSettings.commerceBraintree);
+          $form.data('braintree', commerceBraintree);
+          clearInterval(waitForSdk);
+        }
+      }, 100);
     },
     detach: function (context, settings, trigger) {
       // Detaching on the wrong trigger will clear the Braintree form
@@ -47,10 +44,9 @@
       }
 
       var commerceBraintree = $form.data('braintree');
-      if (commerceBraintree) {
-        commerceBraintree.integration.teardown();
-        $form.removeData('braintree');
-      }
+      commerceBraintree.integration.teardown();
+      $form.removeData('braintree');
+      $form.removeOnce('braintree-attach');
     }
   };
 
@@ -59,93 +55,108 @@
    *
    * @constructor
    */
-  Drupal.commerceBraintree = function($form, settings) {
-    this.settings = settings;
-    this.$form = $form;
-    this.formId = this.$form.attr('id');
+  Drupal.commerceBraintree = function ($form, settings) {
+    var $submit = $form.find('input.button--primary');
+    var that = this;
 
-    this.$form.find('#edit-submit').prop('disabled', false);
-
-    this.$form.bind('submit', function (e) {
-      $(this).find('.messages--error').remove()
-    });
-
-    braintree.setup(settings.clientToken, 'custom', {
-      id: this.formId,
-      hostedFields: settings.hostedFields,
-      onReady: $.proxy(this.onReady, this),
-      onError: $.proxy(this.onError, this),
-      onPaymentMethodReceived: function (payload) {
-        $('.braintree-nonce', $form).val(payload.nonce);
-        $('.braintree-card-type', $form).val(payload.details.cardType);
-        $('.braintree-last2', $form).val(payload.details.lastTwo);
-        $form.submit();
+    braintree.client.create({
+      authorization: settings.clientToken
+    }, function (clientError, clientInstance) {
+      if (clientError) {
+        console.error(clientError);
+        return;
       }
+
+      braintree.hostedFields.create({
+        client: clientInstance,
+        fields: settings.hostedFields
+      }, function (hostedFieldsError, hostedFieldsInstance) {
+        that.hostedFieldsInstance = hostedFieldsInstance;
+        if (hostedFieldsError) {
+          console.error(hostedFieldsError);
+          return;
+        }
+
+        $submit.prop('disabled', false);
+
+        $form.on('submit', function (event, options) {
+          options = options || {};
+          if (options.tokenized) {
+            // Tokenization complete, allow the form to submit.
+            return;
+          }
+
+          event.preventDefault();
+          $('.messages--error', $form).remove();
+
+          hostedFieldsInstance.tokenize(function (tokenizeError, payload) {
+            if (tokenizeError) {
+              console.log(tokenizeError);
+              var message = that.errorMsg(tokenizeError);
+              // Show the message above the form.
+              $form.prepend(Drupal.theme('commerceBraintreeError', message));
+              return;
+            }
+
+            $('.braintree-nonce', $form).val(payload.nonce);
+            $('.braintree-card-type', $form).val(payload.details.cardType);
+            $('.braintree-last2', $form).val(payload.details.lastTwo);
+            $form.trigger('submit', { 'tokenized' : true });
+          });
+        });
+      });
     });
 
     return this;
   };
 
-  Drupal.commerceBraintree.prototype.onReady = function (integration) {
-    this.integration = integration;
-  };
 
-  Drupal.commerceBraintree.prototype.onError = function (response) {
-    if (response.type == 'VALIDATION') {
-      var message = this.errorMsg(response);
-
-      // Show the message above the form.
-      this.$form.prepend(Drupal.theme('commerceBraintreeError', message));
-    }
-    else {
-      console.log('Other error', arguments);
-    }
-  };
-
-  Drupal.commerceBraintree.prototype.errorMsg = function(response) {
+  Drupal.commerceBraintree.prototype.errorMsg = function(tokenizeError) {
     var message;
 
-    switch (response.message) {
-      case 'User did not enter a payment method':
+    switch (tokenizeError.code) {
+      case 'HOSTED_FIELDS_FIELDS_EMPTY':
         message = Drupal.t('Please enter your credit card details.');
         break;
 
-      case 'Some payment method input fields are invalid.':
+      case 'HOSTED_FIELDS_FIELDS_INVALID':
         var fieldName = '';
-        var fields = [];
-        var invalidFields = this.$form.find('.braintree-hosted-fields-invalid');
-
-        if (invalidFields.length > 0) {
-          invalidFields.each(function(index) {
-            var id = $(this).attr('id');
-            // @todo Get the real label.
-            var fieldName = id.replace('-', ' ');
-
-            fields.push(Drupal.t(fieldName));
-          });
-
+        var fields = tokenizeError.details.invalidFieldKeys;
+        if (fields.length > 0) {
           if (fields.length > 1) {
             var last = fields.pop();
             fieldName = fields.join(', ');
             fieldName += ' and ' + Drupal.t(last);
-            message = Drupal.t('The @field you entered are invalid.', {'@field': fieldName});
+            message = Drupal.t('The @fields you entered are invalid.', {'@fields': fieldName});
           }
           else {
             fieldName = fields.pop();
             message = Drupal.t('The @field you entered is invalid.', {'@field': fieldName});
           }
-
         }
         else {
           message = Drupal.t('The payment details you entered are invalid.');
         }
 
         message += ' ' + Drupal.t('Please check your details and try again.');
+        break;
 
+      case 'HOSTED_FIELDS_TOKENIZATION_CVV_VERIFICATION_FAILED':
+        message = Drupal.t('The CVV you entered is invalid.');
+        message += ' ' + Drupal.t('Please check your details and try again.');
+        break;
+
+      case 'HOSTED_FIELDS_FAILED_TOKENIZATION':
+        message = Drupal.t('An error occurred while contacting the payment gateway.');
+        message += ' ' + Drupal.t('Please check your details and try again.');
+        break;
+
+      case 'HOSTED_FIELDS_TOKENIZATION_NETWORK_ERROR':
+        message = Drupal.t('Could not connect to the payment gateway.');
         break;
 
       default:
-        message = response.message;
+        message = tokenizeError.message;
     }
 
     return message;
